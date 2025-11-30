294d25c6f2f18bda4e479b73d9978f58d27769bc
Shuung <qmoy2510@gmail.com>
2025-11-30 15:52:26 +0900

    프로젝트 생성😀

---
+++ src/main/java/com/example/half_step_backend/HalfStepBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.half_step_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HalfStepBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HalfStepBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/half_step_backend/HalfStepBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.half_step_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HalfStepBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
